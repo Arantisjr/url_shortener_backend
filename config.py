a7c6801ff4cff2157281d990282f25c50401535f
Arantisjr <arantisjunior@gmail.com>
2025-08-01 13:39:59 +0100

    chore: add db

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:  # Class name should be capitalized (Config, not config)
     SECRET_KEY = os.environ.get('SECRET_KEY') 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'postgresql://postgres:1234@localhost/url_shortening'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False  
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') 
     JWT_ACCESS_TOKEN_EXPIRES = 7200  # 2 hours
